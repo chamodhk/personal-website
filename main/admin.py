b59016165c49bac77f02d07f20f199228406b363
Chamodh Nethsara <72559423+chamodhk@users.noreply.github.com>
2025-12-22 02:58:13 +0530

    registered the models on the admin site

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+
+admin.site.register(Skill)
+admin.site.register(Certificate)
+admin.site.register(Project)
+admin.site.register(Article)
+
+
+
+
