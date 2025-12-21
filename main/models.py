74bf83ada996ea70e2ae05f10876e4a40e828a91
Chamodh Nethsara <72559423+chamodhk@users.noreply.github.com>
2025-12-22 02:49:22 +0530

    initial commit

---
+++ main/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+
+# Create your models here.
+
+class Skill(models.Model):
+    SKILL_TYPES = [
+        ("language", "Programming Language"),
+        ("framework", "Framework"),
+        ("tool", "Tool"),
+        ("soft", "Soft Skill"),
+        ("other", "Other"),
+    ]
+    name = models.CharField(max_length=50)
+    type = models.CharField(max_length=20, choices=SKILL_TYPES)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Certificate(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+    institute = models.CharField(max_length=50)
+    issued_date = models.DateField(auto_now_add=True)
+    expiry_date = models.DateField(blank=True)
+    skills = models.ManyToManyField(Skill, related_name="certificates")
+
+    def __str__(self) -> str:
+        return f"{self.name} from {self.institute}"
+    
+
+class Project(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.CharField(max_length=1000)
+    completed_date = models.DateField(auto_now_add=True)
+    article_link = models.URLField()
+    github_link = models.URLField()
+    skills = models.ManyToManyField(Skill, related_name="projects")
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Article(models.Model):
+    name = models.CharField(max_length=150)
+    subtitle = models.CharField(max_length=50)
+    date = models.DateField(auto_now_add=True)
+    body = models.TextField()
+    medium_link = models.URLField(blank=True)
+    github_link = models.URLField()
+    youtube_link = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.name
