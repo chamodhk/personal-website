824062ad5d219c11b695bd003208c0feb92d2a37
Chamodh Nethsara <72559423+chamodhk@users.noreply.github.com>
2025-12-22 13:09:25 +0530

    added the home page linked with tailwind css

--- main/views.py
+++ main/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import SiteSettings, Skill, Certificate, Article
 
 # Create your views here.
+
+
+def home(request):
+    settings = SiteSettings.objects.first()
+    articles = Article.objects.order_by('-date')[:3]
+    return render(request, "home.html", {
+        "settings": settings,
+        "articles":articles
+    })
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./main/templates/*.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
